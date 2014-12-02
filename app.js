912d4d57a3d3b55e60e33579b0554e98f38d9fc9
Josh Treadwell <josh.treadwell@gmail.com>
2014-12-01 21:59:52 -0700

    More scaffolding functions based on the discussions from Saturday night

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
-var app = require('http').createServer(handler);
-var url= require('url');
-var fs = require('fs');
-var io = require('socket.io').listen(app);
+var io = require('socket.io')(80);
+var serialport = require("serialport");
 var Dissolve = require('dissolve');
 var Concentrate = require('concentrate');
-var serialport = require("serialport");
-var dgram = require("dgram");
 
-// lib
+// our libs
 var defs = require("./lib/defs.js");
 
-//var enum = require("enum");
 
-var jsonbuff = [];
+// Pre and post buffers
+// these currently just accumulate, but we'll "process" them in a FIFO queue
+
+// The Dissolve parser will push the values it's converted from the Buffer here.  We will
+// want to have another pass on these values to get "readable" information however...
+var jsonBuffArrayIn = [];
+
+
+// We will insert readable JSON here to be sent to the glove.
+// A loop will pull the top array object out, convert it to a Buffer object using Concentrate,
+// and ship it out over a serial port
+var jsonBuffArrayOut = [];
+
+// this waits for the replies before executing the relevant command
+var listenerArray = [];
+
+
+var exec_in = function(jsonBuff){
+    if(jsonBuff.command == defs.out.REPLY_FROM_HOST){
+        defs.in[jsonBuff.command].runIt(jsonBuff);
+    } else {
+        defs.in[jsonBuff.command].runIt(jsonBuff);
+        // change the jsonBuff in to a reply here
+        //exec_out(jsonBuff);
+    }
+}
+
+var exec_out = function(jsonBuff){
+
+    if(jsonBuff.command == defs.out.REPLY_FROM_HOST){
+        // just sending a reply
+    } else
+    {
+        //run concentrate on jsonBuff here....
+
+        //push to listener array
+        listenerArray.push({
+            commandID   :   jsonBuffIn.commandID,
+            obj         :   jsonBuff
+        })
+    }
+}
+
 
 var SP = serialport.SerialPort;
 var serialPort = new SP("/dev/ttyACM0",
@@ -21,14 +58,9 @@ var serialPort = new SP("/dev/ttyACM0",
 		parser: serialport.parsers.readline("\n")
 	}, false);
 
-// NODE byte
-
-var udp_socket = dgram.createSocket('udp4');
-
 
 var parser = Dissolve().loop(function(end) {
     var data_i = 0;
-
     this.uint16be("command")
         .uint16be("commandId")
         .uint16be("totalLength")
@@ -69,15 +101,6 @@ var parser = Dissolve().loop(function(end) {
                              default:       this.buffer("arg", this.vars.argLen);           break;
                     }
                 });
-                //.loop( function(end) {
-                //if (arg_i++ === this.vars.argLen) {
-                //    return end(true);
-                //}
-                //
-                //this.tap(function(){
-                //    this.buffer("arg", arg_Len)
-                //}
-                //});
         })
         .uint8be("checkSum")
         .tap(function() {
@@ -86,25 +109,15 @@ var parser = Dissolve().loop(function(end) {
         });
 });
 
-var processParser = function(JSONbinary) {
-
-    defs.in[JSONbinary.command].runIt(JSONbinary);
-
-    return 0;
-};
 
 parser.on("readable", function() {
     var e;
     while (e = parser.read()) {
-
-        //console.log(JSON.stringify(e, null, 2));
-        //io.sockets.emit(JSON.stringify(e, null, 2));
-        //console.log(e);
-        processParser(e);
-        jsonbuff.push(e);
+        jsonBuffArrayIn.push(e);
     }
 });
 
+// SERIAL PORT ACTIONS
 
 serialPort.open(function (error) {
   if ( error ) {
@@ -123,73 +136,8 @@ serialPort.open(function (error) {
   }
 });
 
-// Http handler function
-function handler (req, res) {
-    
-    // Using URL to parse the requested URL
-    var path = url.parse(req.url).pathname;
-    
-    // Managing the root route
-    if (path == '/') {
-        index = fs.readFile(__dirname+'/public/index.html', 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load index.html");
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/html'});
-                res.end(data);
-            });
 
-    // Managing the route for the javascript files
-    } else if( /\.(js)$/.test(path) ) {
-        index = fs.readFile(__dirname+'/public'+path, 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load " + path);
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/plain'});
-                res.end(data);
-            });
-    } else {
-        res.writeHead(404);
-        res.end("Error: 404 - File not found.");
-    }
-    
-}
-
-
-// Web Socket Connection
-io.sockets.on('connection', function (socket) {
-    
-  // If we recieved a command from a client to start watering lets do so
-  socket.on('ping', function(data) {
-      console.log("ping");
-      
-      delay = data["duration"];
-      
-      // Set a timer for when we should stop watering
-      setTimeout(function(){
-          socket.emit("pong");
-      }, delay * 1);
-      
-  });
-
-});
-
-udp_socket.on('message', function(content, rinfo) {
-	console.log('UDP byte ', content, ' from ', rinfo.address, rinfo.port, ' JSON or rinfo ', JSON.stringify(rinfo));
-	io.sockets.emit('udp_update', content.toString("utf8", 0, rinfo.size));
-});
-
-
-
-// CONCENTRATE STUFF
+// CONCENTRATE (SENDING BINARY)
 
 var c = Concentrate();
 
@@ -209,24 +157,32 @@ c.on("readable", function() {
 // ghetto incrementor
 var counter = 0;
 var randomID = function(){
-    return counter++;
+    counter++
+    return counter;
 };
 
 //assemble a "subscribe" message
 c.uint16be(defs.out.SESS_SUBCRIBE)           //command
-    .uint16be(randomID())                                //commandID
-    .uint16be(12)                               //totalLength
-    .uint8(1)                                 //argCount
-    .uint8(0x07)                                 //argType
-    .uint8(2)                                 //argLen
-    .uint16le(defs.out.LED_WRIST_ON)             // (specific) command code to subscribe to
-    .uint8(0)                                  //checkSum?
-    .flush().end();                             // what is this doing?
+    .uint16be(randomID())                    //commandID
+    .uint16be(12)                            //totalLength
+    .uint8(1)                                //argCount
+    .uint8(defs.arg.a_uint16be)              //argType
+    .uint8(2)                                //argLen
+    .uint16le(defs.out.LED_WRIST_ON)         // (specific) command code to subscribe to
+    .uint8(0)                                //checkSum?
+    .flush();                          // what is this doing?
+
+c.uint16be(defs.out.SESS_SUBCRIBE)
+    .uint16be(randomID())
+    .uint16be(22)
+    .uint8(1)
+    .uint8(defs.arg.a_uint16be)
+    .uint8(2)
+    .uint16le(defs.out.LED_WRIST_OFF)
+    .uint8(0)
+    .flush();
 
 // CONCENTRATE END
-
-
-
 
 
 
@@ -234,7 +190,13 @@ c.uint16be(defs.out.SESS_SUBCRIBE)           //command
 parser.write(new Buffer([0xa0, 0x03, 0x4f, 0xe3, 0x00, 0x0e, 0x01, 0x08, 0x04, 0xea, 0x20, 0x67, 0x00, 0xb6, 0x03, 0x01, 0xf9, 0x5c, 0x00, 0x0b, 0x01, 0x06, 0x01, 0x07, 0xc8, 0xa0, 0x03, 0xad, 0x7d, 0x00, 0x08, 0x00, 0x2a ]));
 
 
+// mainloop... convert this to a Node Eventloop later
 
-// Listens and binds
-app.listen(5000);
-udp_socket.bind(1900);
+shutDown = 0;
+
+while(!shutDown) {
+    if(null != jsonBuffArrayIn[0]) {
+        exec_in(jsonBuffArrayIn[0]);
+        jsonBuffArrayIn.shift();
+    }
+}
