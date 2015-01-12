c53c2b24e4f4fd5940290b268505610e377e7d1e
drquinn <blong123@gmail.com>
2015-01-12 16:47:25 -0700

    fake mag vis on all imus

--- app/js/generateThree.js
+++ app/js/generateThree.js
@@ -6,14 +6,6 @@ Generate 3D render using serial data from IMU
 
 
 // Declare required variables
-var dataRollx = 0;
-var dataRolly = 0;
-var dataRollz = 0;
-var dataRollxArray = [];
-var dataRollyArray = [];
-var dataRollzArray = [];
-var accuracy = 2;
-var orderOfMag = (Math.PI/180);
 var container;
 var camera, scene, renderer;
 var cube, plane;
@@ -22,12 +14,6 @@ var targetRotationOnMouseDown = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-var arrow1 = null;
-var arrow2 = null;
-var vx = 0, vy = 0, vz = 0;
-var dirNew = null;
-var originNew = null;
-var DP_1_mx = 1;
 var imuSet = null;
 var vec = new THREE.Vector3(1,0,0);
 
@@ -45,23 +31,6 @@ function runSocket() {
         socket.on('glove_update', function(data) {
             console.log(data);
             imuSet = data.IMU_set;
-            /*
-            if (data.charAt(0) === 'O') {
-                console.log(data);
-                var dataArray = data.split(/ /);
-
-                // set x
-                dataRollx = (dataArray[1] *= orderOfMag).toFixed(accuracy);
-                
-                // set y
-                dataRolly = (dataArray[2] *= orderOfMag).toFixed(accuracy);
-
-                // set z
-                dataRollz = (dataArray[3] *= orderOfMag).toFixed(accuracy);
-
-                console.log(dataRollx + "," + dataRolly + "," + dataRollz);
-            }
-            */
         });
 }
 
@@ -101,7 +70,7 @@ function init() {
 
     $("#pourHeading").append("<div id='subHeading'></div>");
 
-    // Set up camera
+    // Set up camera.
     camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 );
     
     camera.position.y = 50;
@@ -109,7 +78,7 @@ function init() {
 
     scene = new THREE.Scene();
 
-    // Create spheres 
+    // Create spheres. 
     var geometry = new THREE.SphereGeometry( 20, 20, 20 ); 
     var material = new THREE.MeshBasicMaterial( {color: 'blue' } ); 
     var sphere_DP_1 = new THREE.Mesh( geometry, material ); 
@@ -158,18 +127,36 @@ function init() {
             sphere_CARPALS, sphere_METACARPALS
             );
     
-    // Axis Helper
+    // Axis Helper.
     var axisHelper = new THREE.AxisHelper( 200 );
     scene.add(axisHelper);
 
-    // helper arrow
-    var origin1 = new THREE.Vector3(-300, 300, 0);
-    var direction1 = new THREE.Vector3(-12, -20, 0);
-    arrow_DP_1 = new THREE.ArrowHelper(vec.normalize(), imus.DP_1, 100, 0x884400);
-    arrow_IP_1 = new THREE.ArrowHelper(vec.normalize(), imus.IP_1, 100, 0x884400);
+    // Arrow Helpers to display mag data per IMU.
+    arrow_DP_1 = new THREE.ArrowHelper(vec.normalize(), imus.DP_1, 60, 0x884400);
+    arrow_IP_1 = new THREE.ArrowHelper(vec.normalize(), imus.IP_1, 60, 0x884400);
+    arrow_PP_1 = new THREE.ArrowHelper(vec.normalize(), imus.PP_1, 60, 0x884400);
+    arrow_DP_2 = new THREE.ArrowHelper(vec.normalize(), imus.DP_2, 60, 0x884400);
+    arrow_IP_2 = new THREE.ArrowHelper(vec.normalize(), imus.IP_2, 60, 0x884400);
+    arrow_PP_2 = new THREE.ArrowHelper(vec.normalize(), imus.PP_2, 60, 0x884400);
+    arrow_DP_3 = new THREE.ArrowHelper(vec.normalize(), imus.DP_3, 60, 0x884400);
+    arrow_IP_3 = new THREE.ArrowHelper(vec.normalize(), imus.IP_3, 60, 0x884400);
+    arrow_PP_3 = new THREE.ArrowHelper(vec.normalize(), imus.PP_3, 60, 0x884400);
+    arrow_DP_4 = new THREE.ArrowHelper(vec.normalize(), imus.DP_4, 60, 0x884400);
+    arrow_IP_4 = new THREE.ArrowHelper(vec.normalize(), imus.IP_4, 60, 0x884400);
+    arrow_PP_4 = new THREE.ArrowHelper(vec.normalize(), imus.PP_4, 60, 0x884400);
+    arrow_DP_5 = new THREE.ArrowHelper(vec.normalize(), imus.DP_5, 60, 0x884400);
+    arrow_IP_5 = new THREE.ArrowHelper(vec.normalize(), imus.IP_5, 60, 0x884400);
+    arrow_PP_5 = new THREE.ArrowHelper(vec.normalize(), imus.PP_5, 60, 0x884400);
+    arrow_CARPALS = new THREE.ArrowHelper(vec.normalize(), imus.CARPALS, 60, 0x884400);
+    arrow_METACARPALS = new THREE.ArrowHelper(vec.normalize(), imus.METACARPALS, 60, 0x884400);
 
-
-    scene.add(arrow_DP_1, arrow_IP_2);
+    scene.add(arrow_DP_1, arrow_IP_1, arrow_PP_1,
+            arrow_DP_2, arrow_IP_2, arrow_PP_2,
+            arrow_DP_3, arrow_IP_3, arrow_PP_3,
+            arrow_DP_4, arrow_IP_4, arrow_PP_4,
+            arrow_DP_5, arrow_IP_5, arrow_PP_5,
+            arrow_CARPALS, arrow_METACARPALS
+            );
 
     renderer = new THREE.CanvasRenderer();
     renderer.setClearColor( 0xf0f0f0 );
@@ -194,13 +181,26 @@ function animate() {
         requestAnimationFrame( animate );
         
         var time = Date.now();
-        var directionUpdate = new THREE.Euler();
-        directionUpdate.set( Math.sin( time / 2000 ), Math.cos( time / 3000 ), Math.sin( time / 1000 ), 'XYZ' );
-        arrow1.setDirection( directionUpdate );
-        
-        directionUpdate2 = new THREE.Euler( Math.sin ( DP_1_mx / 2000 ), Math.cos( DP_1_mx / 3000 ), Math.sin ( DP_1_mx / 1000 ), 'XYZ');
-        arrow2.setDirection ( directionUpdate2 );
-        console.log(DP_1_mx);
+        var directionUpdate = new THREE.Euler(Math.sin( time / 2000 ), Math.cos( time / 3000 ), Math.sin( time / 1000 ), 'XYZ' );
+
+        // Update direction on mag arrows.
+        arrow_DP_1.setDirection( directionUpdate );
+        arrow_IP_1.setDirection ( directionUpdate );
+        arrow_PP_1.setDirection ( directionUpdate );
+        arrow_DP_2.setDirection ( directionUpdate );
+        arrow_IP_2.setDirection ( directionUpdate );
+        arrow_PP_2.setDirection ( directionUpdate );
+        arrow_DP_3.setDirection ( directionUpdate );
+        arrow_IP_3.setDirection ( directionUpdate );
+        arrow_PP_3.setDirection ( directionUpdate );
+        arrow_DP_4.setDirection ( directionUpdate );
+        arrow_IP_4.setDirection ( directionUpdate );
+        arrow_PP_4.setDirection ( directionUpdate );
+        arrow_DP_5.setDirection ( directionUpdate );
+        arrow_IP_5.setDirection ( directionUpdate );
+        arrow_PP_5.setDirection ( directionUpdate );
+        arrow_CARPALS.setDirection ( directionUpdate );
+        arrow_METACARPALS.setDirection ( directionUpdate );
 
         render();
 }
