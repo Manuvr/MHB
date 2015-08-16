455192db7b82ee4aec50b24bc401ecff93783224
Zone-Ghost <josh.treadwell@gmail.com>
2015-08-16 02:13:03 -0700

    keep on rollin baby, you know what time it is

--- new/newnew/lib/mCore/messageAction.js
+++ new/newnew/lib/mCore/messageAction.js
@@ -5,7 +5,22 @@ function messageAction() {
 }
 
 messageAction.prototype.process = function(jsonBuff) {
+  switch (jsonBuff.messageId) {
+    case 'LEGEND_MESSAGES':
+      jsonBuff.action = 'update_legend'
+  }
 
+
+};
+
+var derp = {
+  'LEGEND_MESSAGES': {
+    callback: function(args) {
+      merge(this.legendMessage, args)
+    }
+  }
 }
+
+
 
 module.exports = messageAction;
--- new/newnew/test.js
+++ new/newnew/test.js
@@ -2,18 +2,3 @@
 
 var ee = require('events').EventEmitter;
 var inherits = require('util').inherits;
-
-var a = {
-  b: "c",
-  d: "q"
-}
-
-console.log(a);
-
-var derp = function(obj) {
-  delete obj.b;
-  obj.d = "f";
-}
-derp(a);
-
-console.log(a);
