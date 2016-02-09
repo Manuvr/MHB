ae6b87ad8fa475a266220d695ba651cf8e27d8c5
Zone-Ghost <josh.treadwell@gmail.com>
2016-02-09 13:01:40 -0800

    ???

--- broker.js
+++ broker.js
@@ -1,6 +1,12 @@
 var _ = require('lodash');
 var mHub = require('./lib/mHub.js');
 
+// Temporary broker file.  Need to retrofit this and a
+// general JSON schema to mHub to replace this logic...
+// 
+// For demo purposes only
+
+
 // need to remove config requirement
 var config = {
   nothing: "here"
