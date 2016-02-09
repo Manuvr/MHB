5ba859d786b021bfedb94e659163123937230203
Zone-Ghost <josh.treadwell@gmail.com>
2016-02-09 13:47:31 -0800

    readying for demo

--- broker.js
+++ broker.js
@@ -3,7 +3,7 @@ var mHub = require('./lib/mHub.js');
 
 // Temporary broker file.  Need to retrofit this and a
 // general JSON schema to mHub to replace this logic...
-// 
+//
 // For demo purposes only
 
 
@@ -16,20 +16,21 @@ var hub = new mHub(config);
 
 // bounce from here
 hub.on('output', function(message) {
-    // message.target = array
-    // message.data = ???
-    if(
-      message.target[0] !== "_adjunctUpdate" &&
-      message.target[0] !== "_adjunctDef" &&
-      message.target[0] !== "log"
-    ) {
-      console.log("Log or Def: " + message.data)
-    } else {
-      console.log(
-        "Bouncing message [" + message.target.join(", ") + "] :"
+    console.log(
+        "Received: [" + message.target.join(", ") + "] :"
         + JSON.stringify(message.data)
       )
-      hub.emit([message.target, "Deacon", "mHub"], message.data)
-    }
 
+    if(message.target[0] === 'connected') {
+      hub.emit(["assignEngine"], ['NewSession0', 'EddystoneBeacon.js']);
+    }
 });
+
+var local_ip = 0;
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  local_ip = add;
+})
+
+hub.emit(["listen", "tcp.js"], [true, local_ip, 8008]);
+=
